6961d2521893241ad73ac66286106f130d08766d
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-02-13 17:11:49 +0000

    complete table names

--- complete-dbh.bash
+++ complete-dbh.bash
@@ -4,9 +4,32 @@ _dbh()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
+
     opts="ls rm reset"
 
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    case "${prev}" in
+        rm)
+            # read config - from: http://stackoverflow.com/a/4434930/447661
+            configfile=".dbhelperrrc"
+            shopt -s extglob
+            while IFS='= ' read lhs rhs
+            do
+                if [[ $lhs != *( )#* ]]
+                then
+                    declare $lhs=$rhs
+                fi
+            done < "$configfile"
+
+            # get table names
+            local table_names=$(mysql --user=$db_user --password=$db_password -ss -e'show tables' $db_name)
+
+            # complete
+            COMPREPLY=( $(compgen -W "${table_names}" -- ${cur}) )
+            return 0
+            ;;
+
+    esac
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
     return 0
 }
 complete -F _dbh dbh
