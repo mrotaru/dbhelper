691ba667470bc4970de4fe9443a22646572fb4ea
Mihai Rotaru <mihai_rotaru@lavabit.com>
2014-06-23 18:52:53 +0100

    add simple install script

---
+++ bootstrap.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# link to github repository
+remote="https://raw.github.com/mrotaru/dbhelper/master/"
+
+# which files are to be copied
+files=(
+ .dbhelperrc
+ complete-dbhelper.bash
+ dbhelper.bash
+ )
+
+# set download method - curl or wget
+command -v curl >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+   get_method="curl"
+else
+    command -v wget >/dev/null 2>&1
+    [ $? -eq 0 ] && get_method="wget"
+fi
+[ -z "$get_method" ] && { echo "Neither wget nor curl found. Exiting..."; exit 1; }
+
+# download a single file in the current directory
+function get_file()
+{
+    case "$get_method" in
+        "curl") curl -s --write-out "%{url_effective} %{http_code}\n" -O "$1" ;;
+        "wget") wget --no-verbose "$1" -O "$2" ;;
+    esac
+}
+
+# iterate over $files, build URL and download each one
+for file in "${!files[@]}"; do
+    url="${remote}${files[file]}"
+    get_file "$url" "${files[file]}"
+done
