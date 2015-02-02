172a397156fa9b97a09f850d5516bde8452f587b
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-02-02 22:17:31 +0000

    fix prev

--- bootstrap.bash
+++ bootstrap.bash
@@ -26,7 +26,8 @@ function get_file()
         "curl") curl -s --write-out "%{url_effective} %{http_code}\n" -O "$1" ;;
         "wget") wget --no-verbose "$1" -O "$2" ;;
     esac
-    [ ! $? ] && { echo "$get_method failed. Exiting..."; exit 1; } 
+    RET=$?
+    [ $RET -ne 0 ] && { echo "$get_method failed - returned: $RET. Exiting..."; exit 1; } 
 }
 
 # iterate over $files, build URL and download each one
